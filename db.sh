fa7531a34f2e4eba7c9d9c81410163616547b7c2
Lars Riel <larsriel@gmail.com>
2023-10-15 15:16:48 +0200

    updated scripts to use app.js instead of index.js

---
+++ db.sh
@@ -0,0 +1,9 @@
+if [ "$1" = "stop" ]; then
+    docker stop generator_db
+    exit 0
+fi
+
+docker run -d --rm \
+--name generator_db \
+-p 6000:8000 \
+surrealdb/surrealdb:latest start --user root --pass root
