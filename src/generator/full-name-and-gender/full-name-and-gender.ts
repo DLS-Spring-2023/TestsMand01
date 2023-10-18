a049f036b917cbf852d3eaad8ac3e573b9088c2d
Lars Riel <larsriel@gmail.com>
2023-10-18 15:04:00 +0200

    fixed unparsed json

--- src/generator/full-name-and-gender/full-name-and-gender.ts
+++ src/generator/full-name-and-gender/full-name-and-gender.ts
@@ -1,9 +1,14 @@
-import { persons } from "../../data/person-names.json";
+// import { persons } from "../../data/person-names.json";
 // Sometimes it will load the import, sometimes it wont, i've tried looking into encoding problems, ways of importing json files, but nothing seems to work
+import { readFileSync } from 'fs';
 import { FullNameAndGender, } from "../../models";
 import { Gender } from "../../enums/Gender"
 
+const json = readFileSync('./src/data/person-names.json', 'utf8');
+const { persons } = JSON.parse(json);
+
 export const generateFullNameAndGender =(): FullNameAndGender => {
+
   const mindIndex = 0;
   const maxIndex = persons.length - 1;
   const randomIndex = Math.floor(Math.random() * (maxIndex - mindIndex + 1)) + mindIndex;
