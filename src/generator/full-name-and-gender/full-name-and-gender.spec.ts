456f718c3d3ee8d4357f9ed295b0cc5e179d134f
Raszbanana <75260673+Raszbanana@users.noreply.github.com>
2023-10-18 15:24:43 +0200

    fixed possible error in full-name-and-gender.spec

--- src/generator/full-name-and-gender/full-name-and-gender.spec.ts
+++ src/generator/full-name-and-gender/full-name-and-gender.spec.ts
@@ -3,10 +3,12 @@ import { generateFullNameAndGender } from './full-name-and-gender'
 
 describe('generateFullNameAndGender', () => {
   it('should return a random full name and gender', () => {
+    const validGenders = [Gender.Female, Gender.Male]
     const result = generateFullNameAndGender();
+
     expect(result).toHaveProperty('name');
     expect(result).toHaveProperty('surname');
-    expect(result).toHaveProperty('gender', Gender.Female | Gender.Male);
+    expect(validGenders).toContain(result.gender)
 
     expect(result.name).not.toBeNull();
     expect(result.surname).not.toBeNull();
