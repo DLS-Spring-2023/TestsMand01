384863fb6e7545e91c5a2538a2fda4e66cb5b826
Paweł Stępień <pstepien@interia.pl>
2023-10-18 14:04:34 +0200

    added the generate phone number method

---
+++ src/generator/phoneNumber/phoneNumber.spec.ts
@@ -0,0 +1,22 @@
+import { generatePhoneNumber } from './phoneNumber';
+import { phoneNumberStarer } from './phoneNumberStarter.const';
+
+describe('generatePhoneNumber', () => {
+  for (let i = 1; i <= 10; i++) {
+    it('should generate a random phone number', () => {
+      const generatedPhoneNumber = generatePhoneNumber();
+
+      expect(generatedPhoneNumber.toString().length).toEqual(8);
+    });
+
+    it('should start with one of the following values', () => {
+      const generatedPhoneNumber = generatePhoneNumber().toString();
+
+      const startsWithMatch = phoneNumberStarer.some((starter) =>
+        generatedPhoneNumber.startsWith(starter.toString())
+      );
+
+      expect(startsWithMatch).toBeTruthy();
+    });
+  }
+});
---
+++ src/generator/phoneNumber/phoneNumber.ts
@@ -0,0 +1,21 @@
+import { phoneNumberStarer } from './phoneNumberStarter.const';
+
+export const generatePhoneNumber = (): number => {
+  let generatedPhoneNumber = 0;
+
+  const starter =
+    phoneNumberStarer[Math.floor(Math.random() * phoneNumberStarer.length)];
+
+  const digits = 8 - starter.length;
+
+  const minNumber = Math.pow(10, digits - 1);
+  const maxNumber = Math.pow(10, digits) - 1;
+
+  const restOfTheNumber = (
+    Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber
+  ).toString();
+
+  generatedPhoneNumber = parseInt(starter + restOfTheNumber);
+
+  return generatedPhoneNumber;
+};
