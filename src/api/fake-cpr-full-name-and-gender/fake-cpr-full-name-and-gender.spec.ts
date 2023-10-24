1d19f1f0638d64975f1ce5154022da5b67c730a3
Paweł Stępień <pstepien@interia.pl>
2023-10-24 18:13:19 +0200

    added some tests for fake cpr full naem and gender and fake cpr full name gender and date of birth

--- src/api/fake-cpr-full-name-and-gender/fake-cpr-full-name-and-gender.spec.ts
+++ src/api/fake-cpr-full-name-and-gender/fake-cpr-full-name-and-gender.spec.ts
@@ -1,12 +1,20 @@
 import { fakeCprFullNameAndGender } from './fake-cpr-full-name-and-gender';
+import { Gender } from '../../enums/Gender';
 
 describe('fakeCprFullNameAndGender', () => {
 	it('should return the  cpr, full name and gender', () => {
+		const validGenders = [Gender.Female, Gender.Male];
 		const result = fakeCprFullNameAndGender();
 
 		expect(result).toHaveProperty('name');
 		expect(result).toHaveProperty('surname');
 		expect(result).toHaveProperty('gender');
 		expect(result).toHaveProperty('cpr');
+		expect(validGenders).toContain(result.gender)
+		expect(result.name).not.toBeNull();
+		expect(result.surname).not.toBeNull();
+		expect(result.cpr.length).toBe(10)
+
+		
 	});
 });
