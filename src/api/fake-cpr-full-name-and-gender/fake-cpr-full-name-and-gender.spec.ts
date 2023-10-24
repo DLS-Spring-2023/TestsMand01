e7794b6c07f7e9d06558bb4ffbbd9b5e7e30c588
Paweł Stępień <pstepien@interia.pl>
2023-10-24 18:13:54 +0200

    changed the naming to genders on fake cpr full name and gender test

--- src/api/fake-cpr-full-name-and-gender/fake-cpr-full-name-and-gender.spec.ts
+++ src/api/fake-cpr-full-name-and-gender/fake-cpr-full-name-and-gender.spec.ts
@@ -3,14 +3,15 @@ import { Gender } from '../../enums/Gender';
 
 describe('fakeCprFullNameAndGender', () => {
 	it('should return the  cpr, full name and gender', () => {
-		const validGenders = [Gender.Female, Gender.Male];
+		const genders = [Gender.Female, Gender.Male];
 		const result = fakeCprFullNameAndGender();
 
 		expect(result).toHaveProperty('name');
 		expect(result).toHaveProperty('surname');
 		expect(result).toHaveProperty('gender');
 		expect(result).toHaveProperty('cpr');
-		expect(validGenders).toContain(result.gender)
+		
+		expect(genders).toContain(result.gender)
 		expect(result.name).not.toBeNull();
 		expect(result.surname).not.toBeNull();
 		expect(result.cpr.length).toBe(10)
